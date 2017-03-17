f25182de9ea5ef59de91a34e64601203effdd7a4
lijiawen <980026545@qq.com>
2017-03-17 14:38:29 +0800

    尝试push

---
+++ demo2/src/hello/tesy.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by DELL on 2017/3/17.
+ */
+
+public class tesy {
+    public static int powern(int m,int n){
+        for ( int i=0 ;i<n;i++)
+            m=m<<1;
+        return m;
+    }
+    public static  void main( String[] args){
+        System.out.println("3乘8=" + powern(3,4));
+    }
+}
+
